62cff8a13877999cba7665b412a597b0e3833c2c
Michael Hladky <michael@hladky.at>
2019-06-30 21:32:11 +0200

    progress reactive bindings

---
+++ libs/reactive-bindings-and-hooks/feature-shell/src/lib/binding-examples/containers/ivy-life-cycle-decorators-hook-container/ivy-life-cycle-decorators-hook-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rbah-ivy-life-cycle-decorators-hook-container',
+  templateUrl: './ivy-life-cycle-decorators-hook-container.component.html',
+})
+export class IvyLifeCycleDecoratorsHookContainerComponent {
+
+  constructor() { }
+
+}
